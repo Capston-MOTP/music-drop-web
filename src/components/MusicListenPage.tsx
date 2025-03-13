6682c2863cdd0444110f3103732566a678e1abbf
leejiho9898 <cctv1998@naver.com>
2025-03-13 13:18:21 +0900

    feat 사진 적용

--- src/components/MusicListenPage.tsx
+++ src/components/MusicListenPage.tsx
@@ -10,7 +10,7 @@ const MusicListenPage = () => {
       <div className="content">
         <div className="album-cover">
           <img
-            src="https://i.namu.wiki/i/FTz5YpSqz6s6UidL2KsHnmkC6gwUchiHeFM4xae-0ZeOlBFqVKDov54HpL1r1WScWvhJOHi8aKqRkmeQERxBKeEwgeCdfD8x6BipFf-35JIGmG025rLuF7crFuwxv_YcU77pnhFcTVSwvXkHTq5wUg.webp"
+            src="https://image.bugsm.co.kr/album/images/500/3220/322075.jpg"
             alt="Album Cover"
           />
         </div>
