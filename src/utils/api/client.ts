8ac7e3034310a4358bead1b50770ae3a410454f1
leejiho9898 <cctv1998@naver.com>
2025-03-20 04:28:32 +0900

    feat: Add default headers and User-Agent to API client configuration

--- src/utils/api/client.ts
+++ src/utils/api/client.ts
@@ -2,10 +2,17 @@ import axios, { AxiosError } from "axios";
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
+// 헤더 에이전트를 window.navigator.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36";
 apiClient.interceptors.request.use(
   (config) => {
+    config.headers["User-Agent"] =
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.326";
     return config;
   },
   (error) => {
